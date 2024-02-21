15f491f1bca4d83128c12b72c90a61d8247bd2f4
azfcf <azfcf@umsystem.edu>
2024-02-21 16:06:59 -0600

    Add fields and methods to vehicle class

--- Homework4/src/edu/mu/vehicle/Vehicle.java
+++ Homework4/src/edu/mu/vehicle/Vehicle.java
@@ -1,5 +1,27 @@
 package edu.mu.vehicle;
 
-public class Vehicle {
+public abstract class Vehicle {
+	protected String brand;
+	protected String make;
+	protected long modelYear;
+	protected double price;
+	protected VehicleColor color;
+	protected FuelType fuelType;
+	protected double mileage;
+	protected double mass;
+	protected int cylinders;
+	protected double gasTankCapacity;
+	protected StartMechanism startType;
 
+	public abstract double calculateMaintenaceCost(double distance); // calculates maintenance cost for a specific vehicle
+	public abstract double calculateFuelEfficiency(double distance, double fuelPrice); // calculates the engine efficiency
+	public abstract void startEngine(); // prints how the vehicle starts
+	
+	@Override
+	public String toString() {
+		return "Vehicle [brand=" + brand + ", make=" + make + ", modelYear=" + modelYear + ", price=" + price
+				+ ", color=" + color + ", fuelType=" + fuelType + ", mileage=" + mileage + ", mass=" + mass
+				+ ", cylinders=" + cylinders + ", gasTankCapacity=" + gasTankCapacity + ", startType=" + startType
+				+ "]";
+	}
 }
