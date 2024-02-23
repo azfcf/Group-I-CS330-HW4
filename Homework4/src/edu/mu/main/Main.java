e328a62834a938a0a8be772eabfa86cbdce772f8
azfcf <azfcf@umsystem.edu>
2024-02-23 17:27:42 -0600

    Implement main function

--- Homework4/src/edu/mu/main/Main.java
+++ Homework4/src/edu/mu/main/Main.java
@@ -3,7 +3,26 @@ package edu.mu.main;
 import edu.mu.vehicleManager.VehicleManager;
 
 public class Main {
-	VehicleManager man = new VehicleManager();
-	
-	boolean flag = man.initalizeStock();
+	public static void main(String args[]) {
+		// Instantiate vehicleManager, perform operations based on the requirements.
+		VehicleManager vehicleManager = new VehicleManager();
+		
+		// Read vehicle data from the vehicleList.csv file and initialize objects.
+		
+		/*
+		 *  The instructions say to read the list during object initialization.
+		 *  Therefore this isn't implemented in the Main function to avoid initializing
+		 *  the list twice.
+		 */
+		
+		// Display all vehicle information.
+		vehicleManager.displayAllVehicleInformation();
+		
+		// Display all car information.
+		vehicleManager.displayAllCarInformation();
+		
+		// Display all motorbike information.
+		vehicleManager.displayAllMotorBikeInformation();
+	}
 }
+
